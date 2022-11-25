887f12344bc2e744658568195f223f387cafdbce
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2022-11-25 01:03:38 +0100

    response extractor

--- internal/sum/extractors.go
+++ internal/sum/extractors.go
@@ -1,7 +1,11 @@
 package sum
 
 import (
+	"context"
+	"io"
+	"net/http"
 	"net/url"
+	"time"
 )
 
 type URL struct{}
@@ -43,4 +47,56 @@ func (GitHub) Extract(link *Link) {
 	u := link.Facts.Url.Data
 
 	link.Facts.Url.Data = u
+}
+
+type Response struct {
+	Client  *http.Client
+	Timeout time.Duration
+}
+
+func (Response) Name() FactName {
+	return "response"
+}
+
+func (Response) Deps() []FactName {
+	return []FactName{"url"}
+}
+
+func (r *Response) Extract(link *Link) {
+	ctx := context.Background()
+	if r.Timeout != 0 {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithTimeout(ctx, r.Timeout)
+		defer cancel()
+	}
+
+	start := time.Now()
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, link.Facts.Url.Data.String(), nil)
+	if err != nil {
+		return
+	}
+
+	resp, err := r.Client.Do(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	duration := time.Since(start)
+
+	link.Facts.Response = Fact[ResponseData]{
+		HasData: true,
+		Data: ResponseData{
+			Protocol:   [2]int{resp.ProtoMajor, resp.ProtoMinor},
+			Duration:   duration,
+			StatusCode: resp.StatusCode,
+			Body:       body,
+			Headers:    resp.Header,
+		},
+	}
 }
--- internal/sum/structs.go
+++ internal/sum/structs.go
@@ -2,6 +2,7 @@ package sum
 
 import (
 	"net/url"
+	"time"
 
 	"github.com/kazhuravlev/awesome-tool/internal/source"
 )
@@ -24,12 +25,20 @@ type Link struct {
 
 type LinkFacts struct {
 	Url      Fact[*url.URL]
-	Response Fact[struct{}]
+	Response Fact[ResponseData]
 	Github   Fact[struct{}]
-	Gitlab   Fact[struct{}]
 }
 
 type Fact[T any] struct {
 	HasData bool
 	Data    T
+}
+
+type ResponseData struct {
+	// {2, 0} means http 2/0
+	Protocol   [2]int
+	Duration   time.Duration
+	StatusCode int
+	Body       []byte
+	Headers    map[string][]string
 }
