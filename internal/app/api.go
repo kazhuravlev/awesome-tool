b6021eb0b8608d3632153fa10bfff680ac5274ec
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2022-11-27 17:02:28 +0100

    parse checks

--- internal/app/api.go
+++ internal/app/api.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"fmt"
 	"github.com/kazhuravlev/awesome-tool/internal/errorsh"
+	"github.com/kazhuravlev/awesome-tool/internal/rules"
 	"github.com/kazhuravlev/awesome-tool/internal/source"
 	"github.com/kazhuravlev/awesome-tool/internal/sum"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -18,6 +20,39 @@ func Run(ctx context.Context, filename string) error {
 
 	if err := source.Validate(*sourceObj); err != nil {
 		return errorsh.Wrap(err, "validate source object")
+	}
+
+	checks := make(map[string]rules.Check)
+	for _, rule := range sourceObj.Rules {
+		for _, checkStringRaw := range rule.Checks {
+			checkName, checkArgs, err := rules.ParseCheck(checkStringRaw)
+			if err != nil {
+				return errorsh.Wrapf(err, "parse check '%s'", checkStringRaw)
+			}
+
+			switch checkName {
+			default:
+				return errorsh.Newf("unknown check '%s'", checkName)
+			case "response:status-eq":
+				statusCode, err := strconv.Atoi(checkArgs)
+				if err != nil {
+					return errorsh.Wrapf(err, "bad arguments for check '%s'", checkName)
+				}
+
+				checks[checkStringRaw] = rules.CheckResponseStatusEq{
+					ExpectedStatus: statusCode,
+				}
+			case "github-repo:stars-min":
+				minimumStars, err := strconv.Atoi(checkArgs)
+				if err != nil {
+					return errorsh.Wrapf(err, "bad arguments for check '%s'", checkName)
+				}
+
+				checks[checkStringRaw] = rules.CheckResponseStatusEq{
+					ExpectedStatus: minimumStars,
+				}
+			}
+		}
 	}
 
 	// NOTE: register extractors
