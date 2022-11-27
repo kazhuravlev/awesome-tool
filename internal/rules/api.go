37b5e98fedef58ad01bd4175963b41828f943467
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2022-11-27 17:01:29 +0100

    check parser

--- internal/rules/api.go
+++ internal/rules/api.go
@@ -1,10 +1,14 @@
 package rules
 
 import (
+	"errors"
 	"fmt"
+	"regexp"
 
 	"github.com/kazhuravlev/just"
 )
+
+var reCheck = regexp.MustCompile(`^(?P<name>[-\w]+:[-\w]+)\((?P<args>[\w\d,]+)\)$`)
 
 var gChecks map[CheckName]Check
 
@@ -21,4 +25,20 @@ func MustRegisterCheck(check Check) {
 	if err := RegisterCheck(check); err != nil {
 		panic("registering check: " + err.Error())
 	}
+}
+
+func ParseCheck(s string) (CheckName, string, error) {
+	match := reCheck.FindStringSubmatch(s)
+	if len(match) != len(reCheck.SubexpNames()) {
+		return "", "", errors.New("check is not valid")
+	}
+
+	result := make(map[string]string)
+	for i, name := range reCheck.SubexpNames() {
+		if i != 0 && name != "" {
+			result[name] = match[i]
+		}
+	}
+
+	return CheckName(result["name"]), result["args"], nil
 }
